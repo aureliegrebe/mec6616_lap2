4c9f4c799305ab9f9d8b1d14d2c64a54081a2b10
Aurélie Grebe <aurelie.grebe@usherbrooke.ca>
2026-08-27 14:39:07 -0400

    Add analytical solutions for 4.1 and 4.2

--- lap1.py
+++ lap1.py
@@ -1,38 +1,68 @@
 # Résoudre les examples 4.1, 4.2 et 4.3 de Versteeg
 
 import numpy as np
-from solveur import solve, Settings, BC
+from solveur import  solve, Settings, BC
 import matplotlib.pyplot as plt
 
 N_LIST = [5, 10, 20, 50, 100]
+N_LIST_PLOT = [5, 10, 20]
+
+# Ex 4.1 
+THERM_COND1 = 1000 # (W / m K)
+AIRE1 = 10e-3 # m²
+L1 = 0.5 # m
+T_A1 = 100
+T_B1 = 500
+
+# Ex 4.2
+T_A2 = 100
+T_B2 = 200
+L2 = 0.02
+THERM_COND2 = 0.5 # W / mK
+Q2 = 1e6 # W/m^3
+
+N_ANALYTIQUE = 100
+
+sol_analytique_4_1 = lambda x: 800 * x + 100
+sol_analytique_4_2 = lambda x: ((T_B2 - T_A2) / L2 + Q2 / 2 / THERM_COND2 * (L2 - x))* x + T_A2
 
 def ex4_1():
     param = Settings()
-    param.right_BC = BC("DIRICHLET", 500)
-    T = solve(param)
+    param.therm_cond = THERM_COND1
+    param.aire = AIRE1
+    param.left_BC = BC("DIRICHLET", T_A1)
+    param.right_BC = BC("DIRICHLET", T_B1)
 
     plt.figure("ex 4.1")
-    for n in N_LIST:
+    x = np.linspace(0, L1, N_ANALYTIQUE)
+    plt.plot(x, sol_analytique_4_1(x), label="Solution analytique")
+    for n in N_LIST_PLOT:
         param.n = n
         X, T = solve(param)
-        plt.plot(X, T, label=f"n = {n}")
+        plt.plot(X, T, "o", label=f"n = {n}")
+    plt.xlabel("x (m)")
+    plt.ylabel("T (°C)")
     plt.legend()
 
 def ex4_2():
     param = Settings()
-    param.length = 0.02
-    param.therm_cond = 0.5
+    param.length = L2
+    param.therm_cond = THERM_COND2
     param.aire = 1
-    param.q_u = 1e6
-    param.left_BC = BC("DIRICHLET", 100)
-    param.right_BC = BC("DIRICHLET", 200)
+    param.q_u = Q2
+    param.left_BC = BC("DIRICHLET", T_A2)
+    param.right_BC = BC("DIRICHLET", T_B2)
     T = solve(param)
 
     plt.figure("ex 4.2")
-    for n in N_LIST:
+    x = np.linspace(0, L2, N_ANALYTIQUE)
+    plt.plot(x, sol_analytique_4_2(x), label="Solution analytique")
+    for n in N_LIST_PLOT:
         param.n = n
         X, T = solve(param)
-        plt.plot(X, T, label=f"n = {n}")
+        plt.plot(X, T, "o", label=f"n = {n}")
+    plt.xlabel("x (m)")
+    plt.ylabel("T (°C)")
     plt.legend()
 
 def ex4_3():
@@ -42,7 +72,7 @@ def ex4_3():
     param.aire = 1
     param.q_u = 1e6
     param.left_BC = BC("DIRICHLET", 100)
-    param.right_BC = BC("NEUMAN", 0)
+    param.right_BC = BC("NEUMANN", 0)
     T = solve(param)
 
     plt.figure("ex 4.3")
@@ -53,6 +83,7 @@ def ex4_3():
     plt.legend()
 
 def main():
+    ex4_1()
     ex4_2()
     plt.show()
 
